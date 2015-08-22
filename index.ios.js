f5fee192633f909420269e4b3414815d40a06221
besartshyti <besartshyti@gmail.com>
2015-08-22 21:50:55 +0100

    move element around

--- index.ios.js
+++ index.ios.js
@@ -5,7 +5,8 @@ var {
   AppRegistry,
   StyleSheet,
   Animated,
-  View
+  View,
+  PanResponder
 } = React;
 
 var SQUARE_DIM = 100;
@@ -17,6 +18,22 @@ var Square = React.createClass({
       pan: new Animated.ValueXY()
     };
   },
+  componentWillMount () {
+    this._panResponder = PanResponder.create({
+        onMoveShoudSetResponderCapture: () => true,
+        onMoveShouldSetPanResponderCapture: () => true,
+        onPanResponderGrunt: (e, gestureState) => {
+            this.state.pan.setOffset({x: this.state.pan.x.getAnimatedValue(), y: this.state.pan.y.getAnimatedValue()});
+            this.state.pan.setValue({x: 0, y: 0});
+        },
+        onPanResponderMove: Animated.event([
+            null, {dx: this.state.pan.x, dy: this.state.pan.y}
+        ]),
+        onPanResponderRelease: () => {
+            this.state.pan.flattenOffset();
+        }
+    });
+  },
   getStyle () {
     return [
         styles.square,
@@ -27,7 +44,10 @@ var Square = React.createClass({
 
     return (
       <View style={styles.container}>
-        <Animated.View style={this.getStyle()} />
+        <Animated.View
+            style={this.getStyle()}
+            {...this._panResponder.panHandlers}
+        />
       </View>
     );
   }
