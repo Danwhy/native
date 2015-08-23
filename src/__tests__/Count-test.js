fa6d87f6e67b83e7d66b2068634b0709b83786ce
besartshyti <besartshyti@gmail.com>
2015-08-23 16:50:42 +0100

    something working

--- src/__tests__/Count-test.js
+++ src/__tests__/Count-test.js
@@ -1,9 +1,17 @@
 'use strict';
 
+jest.dontMock('../Count.js');
+
+var React = require('react/addons');
+var TestUtils = React.addons.TestUtils;
+var Count = require('../Count.js');
+
 describe('This is a test', () => {
 
     it('should pass', () => {
 
+        console.log('ENV', __DEV__);
+        console.log('Component', Count);
         expect(true).toBe(true);
     });
 });
