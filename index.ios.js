b476b1ce2e8c25cca3d054a00ec68310f16556ec
besartshyti <besartshyti@gmail.com>
2015-08-23 13:25:31 +0100

    first draft with many cards

--- index.ios.js
+++ index.ios.js
@@ -9,32 +9,99 @@ var {
   PanResponder
 } = React;
 
+var Card = [
+    'red',
+    'green',
+    'blue',
+    'purple',
+    'orange'
+];
+
 var SQUARE_DIM = 100;
+var SWIPE_THRESHOLD = 120;
 
 var Square = React.createClass({
 
   getInitialState () {
     return {
-      pan: new Animated.ValueXY()
+      pan: new Animated.ValueXY(),
+      enter: new Animated.Value(0.5),
+      card: Card[0]
     };
   },
+  goToNextCard () {
+    let currentCardIdx = Card.indexOf(this.state.card);
+    let newIdx = currentCardIdx + 1;
+
+    this.setState({
+        card: Card[newIdx > Card.length - 1 ? 0 : newIdx]
+    });
+  },
+  componentDidMount () {
+    this.animateEntrance();
+  },
+  animateEntrance () {
+    Animated.spring(this.state.enter, {
+        toValue: 1,
+        friction: 8
+    }).start();
+  },
   componentWillMount () {
+
+    // this.state.pan.x._value === this.state.x.getAnimatedValue()
+    console.log('pan.setOffset', this.state.pan.x._value);
+    console.log('pan.setOffset', this.state.pan.x.getAnimatedValue());
+
     this._panResponder = PanResponder.create({
         onMoveShoudSetResponderCapture: () => true,
         onMoveShouldSetPanResponderCapture: () => true,
         onPanResponderGrunt: (e, gestureState) => {
-            this.state.pan.setOffset({x: this.state.pan.x.getAnimatedValue(), y: this.state.pan.y.getAnimatedValue()});
+            this.state.pan.setOffset({
+                x: this.state.pan.x.getAnimatedValue(),
+                y: this.state.pan.y.getAnimatedValue()
+            });
             this.state.pan.setValue({x: 0, y: 0});
         },
         onPanResponderMove: Animated.event([
             null, {dx: this.state.pan.x, dy: this.state.pan.y}
         ]),
-        onPanResponderRelease: () => {
-            Animated.spring(this.state.pan, {
-                toValue: 0
-            }).start();
+        onPanResponderRelease: (e, gestureState) => {
+
+            this.state.pan.flattenOffset();
+            let {vx, vy} = gestureState;
+
+            console.log('gestureState', vx, vy);
+
+            if (Math.abs(this.state.pan.x.getAnimatedValue()) > SWIPE_THRESHOLD) {
+                Animated.decay(this.state.pan.x, {
+                    velocity: 1
+                }).start(() => {
+
+                    console.log('Done x');
+                    this.resetState();
+                }.bind(this));
+
+                Animated.decay(this.state.pan.y, {
+                    velocity: 1
+                }).start(() => {
+
+                    console.log('Done y');
+                    this.resetState();
+                }.bind(this));
+            } else {
+                Animated.spring(this.state.pan, {
+                    toValue: {x: 0, y: 0},
+                    friction: 4
+                }).start();
+            }
         }
     });
+  },
+  resetState () {
+    this.state.pan.setValue({x: 0, y: 0});
+    this.state.enter.setValue(0);
+    this.goToNextCard();
+    this.animateEntrance();
   },
   getStyle () {
     return [
@@ -68,8 +135,14 @@ var Square = React.createClass({
     return (
       <View style={styles.container}>
         <Animated.View
-            style={this.getStyle()}
-            {...this._panResponder.panHandlers}
+            style={
+                [
+                    styles.card,
+                    animatedCardStyles,
+                    {backgroundColor: this.state.card}
+                ]
+                {...this._panResponder.panHandlers}
+            }
         />
       </View>
     );
@@ -82,7 +155,7 @@ var styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center'
     },
-    square: {
+    card: {
         width: SQUARE_DIM,
         height: SQUARE_DIM,
         backgroundColor: 'blue'
